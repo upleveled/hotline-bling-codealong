7d7d5205537717fd6c205413f5b26fb6a6f47864
Karl Horky <karl.horky@gmail.com>
2022-02-19 16:56:42 +0100

    Upgrade packages

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React, { Fragment } from 'react';
 import { StyleSheet } from 'react-native';
 import Home from './screens/Home';
 import New from './screens/New';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const forFade = ({ current }) => ({
   cardStyle: {
