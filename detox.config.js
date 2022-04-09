80b7fc6b5a46cf78c4a01fe0bf7da834fb0685d2
Karl Horky <karl.horky@gmail.com>
2022-04-09 16:00:37 +0200

    Clean up Detox config

--- detox.config.js
+++ detox.config.js
@@ -5,22 +5,33 @@ module.exports = {
   apps: {
     ios: {
       type: 'ios.app',
-      // build:
-      //   'eas build --platform ios --profile simulator --non-interactive --local --output=e2e/bin/hotlineblingcodealong.app',
-      // binaryPath: './e2e/bin/hotlineblingcodealong.app',
+
+      // Run xcodebuild and exit with non-zero if the build fails
       build: './scripts/build-detox-ios.sh hotlineblingcodealong Release YES',
       binaryPath:
         'ios/build/Build/Products/Release-iphonesimulator/hotlineblingcodealong.app',
+
+      // FIXME: As of April 2022, EAS builds (both local and cloud)
+      // do not work with Detox
+      // build:
+      //   'eas build --platform ios --profile simulator --non-interactive --local --output=e2e/bin/hotlineblingcodealong.app',
+      // binaryPath: './e2e/bin/hotlineblingcodealong.app',
     },
+
+    // TODO: Set up Android configuration and tests
     android: {
       type: 'android.apk',
-      build:
-        'eas build --platform=android --profile emulator --non-interactive --local --output=e2e/bin/app-test.apk',
+      // build:
+      //   'eas build --platform=android --profile emulator --non-interactive --local --output=e2e/bin/app-test.apk',
       // These both need to be configured for Detox to not
       // auto-generate an incorrect, nonexistent path
       // https://github.com/wix/Detox/issues/2274
-      binaryPath: 'e2e/bin/app-test.apk',
-      testBinaryPath: 'e2e/bin/app-test.apk',
+      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      build:
+        'pushd android; ./gradlew app:assembleRelease app:assembleAndroidTest -DtestBuildType=release; popd',
+      // binaryPath: 'e2e/app.apk',
+      // testBinaryPath:
+      //   'e2e/89fca8e0-b364-4195-9674-13c0a4394713-bc26edcca6de40eb9c56272be54e70db.apk',
     },
   },
   devices: {
