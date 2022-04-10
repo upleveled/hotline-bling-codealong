adcbfcba24bfffa14965c6e286ec96b790cf5373
Karl Horky <karl.horky@gmail.com>
2022-04-10 19:09:03 +0200

    Add missing Jest config

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  maxWorkers: 1,
-  testEnvironment: './environment',
-  testRunner: 'jest-circus/runner',
+  testEnvironment: require.resolve('./environment.js'),
+  testRunner: require.resolve('jest-circus/runner'),
   testTimeout: 120000,
   testRegex: '\\.e2e\\.js$',
-  reporters: ['detox/runners/jest/streamlineReporter'],
+  reporters: [require.resolve('detox/runners/jest/streamlineReporter')],
   verbose: true,
 };
