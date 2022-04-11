42cffec339d4e9cc14a032a5e95c7b64b22689b9
Karl Horky <karl.horky@gmail.com>
2022-04-11 11:43:50 +0200

    Adapt comment

--- e2e/appStarts.e2e.js
+++ e2e/appStarts.e2e.js
@@ -12,8 +12,8 @@ describe('Example', () => {
   it('should have header element', async () => {
     await waitFor(element(by.id('HeaderView')))
       .toBeVisible()
-      // Wait for element for 30 seconds to
-      // work around "System UI isn't responding" ANR
+      // Wait for element for 30 seconds to work
+      // around "System UI isn't responding" ANR
       // (Application Not Responding) error
       // https://github.com/ReactiveCircus/android-emulator-runner/issues/129
       // https://github.com/upleveled/hotline-bling-codealong/pull/26#issuecomment-1094659722
