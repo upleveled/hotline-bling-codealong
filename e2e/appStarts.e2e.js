7caa1612994e666517f01ef0f2e40552138f389e
Karl Horky <karl.horky@gmail.com>
2022-04-11 11:20:23 +0200

    Add comments

--- e2e/appStarts.e2e.js
+++ e2e/appStarts.e2e.js
@@ -12,6 +12,11 @@ describe('Example', () => {
   it('should have header element', async () => {
     await waitFor(element(by.id('HeaderView')))
       .toBeVisible()
+      // Wait for element for 30 seconds to
+      // work around "System UI isn't responding" ANR
+      // (Application Not Responding) error
+      // https://github.com/ReactiveCircus/android-emulator-runner/issues/129
+      // https://github.com/upleveled/hotline-bling-codealong/pull/26#issuecomment-1094659722
       .withTimeout(30000);
     await expect(element(by.id('HeaderView'))).toBeVisible();
   });
