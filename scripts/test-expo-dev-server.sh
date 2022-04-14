d6d43e7a5be812862873feb300a442587eb91f8b
Karl Horky <karl.horky@gmail.com>
2022-04-14 23:04:49 +0200

    Add more debugging diff --git a/scripts/test-expo-dev-server.sh b/scripts/test-expo-dev-server.sh index 0336cc3..e958bbe 100755 --- a/scripts/test-expo-dev-server.sh +++ b/scripts/test-expo-dev-server.sh @@ -8,6 +8,7 @@ touch expo-start.log  # Run Expo dev server, redirecting stdout + stderr to log file  expo start --android --non-interactive > expo-start.log 2>&1 &  expo_start_pid=$! +echo "expo start pid: $expo_start_pid"
     # Race processes to locate success + failure messages in dev server output
     # Ref: https://superuser.com/a/1074656/157255
    @@ -22,13 +23,21 @@ echo waiting
     # it was the Expo dev server process, continue
     # waiting for the next process to finish
     wait -n
    +waited_pid=$!
     [ $! -eq $expo_start_pid ] && wait -n
    +waited_pid_2=$!
    
     # Get the exit code of the process, kill the
     # remaining processes, and exit with the
     # same exit code
     exit_code=$?
     echo "exit code: $exit_code"
    +echo "waited pid: $waited_pid"
    +echo "waited pid 2: $waited_pid_2"
    +
    +
    +cat expo-start.log
    +
     pkill -P $$
     echo after wait
     exit $exit_code

--- scripts/test-expo-dev-server.sh
+++ scripts/test-expo-dev-server.sh
@@ -8,6 +8,7 @@ touch expo-start.log
 # Run Expo dev server, redirecting stdout + stderr to log file
 expo start --android --non-interactive > expo-start.log 2>&1 &
 expo_start_pid=$!
+echo "expo start pid: $expo_start_pid"
 
 # Race processes to locate success + failure messages in dev server output
 # Ref: https://superuser.com/a/1074656/157255
@@ -22,13 +23,21 @@ echo waiting
 # it was the Expo dev server process, continue
 # waiting for the next process to finish
 wait -n
+waited_pid=$!
 [ $! -eq $expo_start_pid ] && wait -n
+waited_pid_2=$!
 
 # Get the exit code of the process, kill the
 # remaining processes, and exit with the
 # same exit code
 exit_code=$?
 echo "exit code: $exit_code"
+echo "waited pid: $waited_pid"
+echo "waited pid 2: $waited_pid_2"
+
+
+cat expo-start.log
+
 pkill -P $$
 echo after wait
 exit $exit_code
