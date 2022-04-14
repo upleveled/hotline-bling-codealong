5b852c6f40286d8942b74dcf7f7a518396144bc0
Karl Horky <karl.horky@gmail.com>
2022-04-14 21:43:13 +0200

    Improve waiting, add messages

--- scripts/test-expo-dev-server.sh
+++ scripts/test-expo-dev-server.sh
@@ -2,19 +2,23 @@
 
 #  Run Expo dev server, redirecting stdout + stderr to log file
 yarn start --android > expo-start.log 2>&1 &
+expo_start_pid=$!
 
-# Race jobs to locate success + error messages in dev server output
+# Race processes to locate success + failure messages in dev server output
 # Ref: https://superuser.com/a/1074656/157255
 # Ref: https://unix.stackexchange.com/a/231678/86691
-{ timeout 120 grep -m 1 "Android Bundling complete" <(tail -f expo-start.log); } &
-{ timeout 120 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && cat expo-start.log && exit 1; } &
+{ timeout 120 grep -m 1 "Android Bundling complete" <(tail -f expo-start.log) && echo "Expo dev server start succeeded"; } &
+{ timeout 120 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && echo "Expo dev server start failed" && cat expo-start.log && exit 1; } &
 
-# Wait for the server start to finish
+# Wait for the first process to finish, and if
+# it was the Expo dev server process, continue
+# waiting for the next process to finish
 wait -n
+[ $! -eq $expo_start_pid ] && wait -n
 
-# Wait for the first grep job to complete
-# and then kill the remaining job
-wait -n
+# Get the exit code of the process, kill the
+# remaining processes, and exit with the
+# same exit code
 exit_code=$?
 pkill -P $$
 exit $exit_code
