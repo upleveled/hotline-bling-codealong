62c61d6204750f6018e60cdadb38fce27edd3bbe
Karl Horky <karl.horky@gmail.com>
2022-04-14 23:25:41 +0200

    Increase timeout

--- scripts/test-expo-dev-server.sh
+++ scripts/test-expo-dev-server.sh
@@ -16,8 +16,8 @@ tail_pid=$!
 # Race processes to locate success + failure messages in dev server output
 # Ref: https://superuser.com/a/1074656/157255
 # Ref: https://unix.stackexchange.com/a/231678/86691
-{ timeout 120 grep -m 1 "Android Bundling complete" <(tail -f expo-start.log) && echo "Expo dev server start succeeded"; } &
-{ timeout 120 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && echo "Expo dev server start failed" || echo "Timeout reached" && (cat expo-start.log && exit 1); } &
+{ timeout 240 grep -m 1 "Android Bundling complete" <(tail -f expo-start.log) && echo "Expo dev server start succeeded"; } &
+{ timeout 240 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && echo "Expo dev server start failed" || echo "Timeout reached" && (cat expo-start.log && exit 1); } &
 
 
 echo waiting
