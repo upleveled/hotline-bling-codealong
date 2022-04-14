17c84b8f7965fad8d64301fbfc80b3489f527ca4
Karl Horky <karl.horky@gmail.com>
2022-04-14 23:00:01 +0200

    Add back exit instead of variable

--- scripts/test-expo-dev-server.sh
+++ scripts/test-expo-dev-server.sh
@@ -7,13 +7,11 @@ echo starting
 expo start --android --non-interactive > expo-start.log 2>&1 &
 expo_start_pid=$!
 
-exit_code=0
-
 # Race processes to locate success + failure messages in dev server output
 # Ref: https://superuser.com/a/1074656/157255
 # Ref: https://unix.stackexchange.com/a/231678/86691
 { timeout 120 grep -m 1 "Android Bundling complete" <(tail -f expo-start.log) && echo "Expo dev server start succeeded"; } &
-{ timeout 120 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && echo "Expo dev server start failed" && cat expo-start.log && exit_code=1; } &
+{ timeout 120 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && echo "Expo dev server start failed" && cat expo-start.log && exit 1; } &
 
 
 echo waiting
@@ -24,11 +22,11 @@ echo waiting
 wait -n
 [ $! -eq $expo_start_pid ] && wait -n
 
-
 # Get the exit code of the process, kill the
 # remaining processes, and exit with the
 # same exit code
-# exit_code=$?
+exit_code=$?
+echo "exit code: $exit_code"
 pkill -P $$
 echo after wait
 exit $exit_code
