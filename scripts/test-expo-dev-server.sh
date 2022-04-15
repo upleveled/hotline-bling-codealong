fbd7f2c6b41c31b0f6cae1878f70c6e0e3e287db
Karl Horky <karl.horky@gmail.com>
2022-04-15 10:20:34 +0200

    Try new approach with loop

--- scripts/test-expo-dev-server.sh
+++ scripts/test-expo-dev-server.sh
@@ -12,24 +12,50 @@ expo_start_pid=$!
 tail -f expo-start.log &
 tail_pid=$!
 
+# trap "kill $tail_pid && kill $expo_start_pid" SIGINT SIGTERM EXIT
+
+timeout 360 tail -f expo-start.log | while read LOGLINE
+do
+  if [[ "${LOGLINE}" == *"Android Bundling complete"* || "${LOGLINE}" == *"Android Bundling failed"* ]]
+  then
+    exit_code=0
+
+    if [[ "${LOGLINE}" == *"Android Bundling complete"* ]]
+    then
+      echo "Expo dev server start succeeded"
+    elif [[ "${LOGLINE}" == *"Android Bundling failed"* ]]
+    then
+      echo "Expo dev server start failed"
+      exit_code=1
+    fi
+
+    kill ${tail_pid}
+    kill ${expo_start_pid}
+    exit ${exit_code}
+  fi
+done || ([ $? -eq 124 ] && echo "Timed out waiting for Expo dev server start (after 360 seconds)")
+
+
 # Race processes to locate success + failure messages in dev server output
 # Ref: https://superuser.com/a/1074656/157255
 # Ref: https://unix.stackexchange.com/a/231678/86691
-{ timeout 310 grep -m 1 "Android Bundling complete" <(tail -f expo-start.log) && echo "Expo dev server start succeeded"; } &
-{ timeout 300 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && echo "Expo dev server start failed" || echo "Timeout reached" && (cat expo-start.log && exit 1); } &
+#todo: combine
+# { timeout 310 grep -m 1 "Android Bundling complete" <(tail -f expo-start.log) && echo "Expo dev server start succeeded"; } &
+# { timeout 300 grep -m 1 "Android Bundling failed" <(tail -f expo-start.log) && echo "Expo dev server start failed" || echo "Timeout reached" && (cat expo-start.log && exit 1); } &
 
-# Wait for the first process to finish, and if
-# it was the Expo dev server process or the tail
-# process, continue waiting for the next process
-# to finish
-wait -n
-([ $! -eq $expo_start_pid ] || [ $! -eq $tail_pid ]) && wait -n
-([ $! -eq $expo_start_pid ] || [ $! -eq $tail_pid ]) && wait -n
+# # Wait for the first process to finish, and if
+# # it was the Expo dev server process or the tail
+# # process, continue waiting for the next process
+# # to finish
+# #todo: wait only on grep
+# wait -n
+# ([ $! -eq $expo_start_pid ] || [ $! -eq $tail_pid ]) && wait -n
+# ([ $! -eq $expo_start_pid ] || [ $! -eq $tail_pid ]) && wait -n
 
-# Get the exit code of the process, kill the
-# remaining processes, and exit with the
-# same exit code
-exit_code=$?
-echo "Exiting with exit code $exit_code"
-pkill -P $$
-exit $exit_code
+# # Get the exit code of the process, kill the
+# # remaining processes, and exit with the
+# # same exit code
+# exit_code=$?
+# echo "Exiting with exit code $exit_code"
+# pkill -P $$
+# exit $exit_code
