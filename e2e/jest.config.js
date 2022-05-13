bc7aea1710ecc77c0197c72180396a9695146f03
Karl Horky <karl.horky@gmail.com>
2022-05-13 16:28:27 +0200

    Fix testRegex

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: require.resolve('./environment.js'),
   transform: {},
   testTimeout: 120000,
-  testRegex: '\\.e2e\\.js$',
+  testRegex: '\\.e2e\\.mjs$',
   reporters: [require.resolve('detox/runners/jest/streamlineReporter')],
   verbose: true,
 };
