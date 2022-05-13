75163cb1da0894d5601474b88e77510007dc78fb
Karl Horky <karl.horky@gmail.com>
2022-05-13 12:44:20 +0200

    Disable transforms

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: require.resolve('./environment.js'),
   testRunner: require.resolve('jest-circus/runner'),
+  transform: {},
   testTimeout: 120000,
   testRegex: '\\.e2e\\.js$',
   reporters: [require.resolve('detox/runners/jest/streamlineReporter')],
