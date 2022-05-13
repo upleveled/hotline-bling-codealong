464003895e4749af460648dd120c58e1f7866ba3
Karl Horky <karl.horky@gmail.com>
2022-05-13 12:48:18 +0200

    Disable old jest-circus/runner

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: require.resolve('./environment.js'),
-  testRunner: require.resolve('jest-circus/runner'),
   transform: {},
   testTimeout: 120000,
   testRegex: '\\.e2e\\.js$',
