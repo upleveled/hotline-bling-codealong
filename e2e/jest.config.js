e1eb951fb5174c7f97805393c1fbe99dffc88f9b
Karl Horky <karl.horky@gmail.com>
2022-11-10 23:30:35 +0100

    Switch to rootDir / testMatch

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   maxWorkers: 1,
   testEnvironment: require.resolve('./environment.js'),
   testTimeout: 120000,
-  testRegex: '\\.e2e\\.ts$',
+  rootDir: '..',
+  testMatch: ['<rootDir>/e2e/**/*.e2e.ts'],
   transform: {
     '\\.tsx?$': 'ts-jest',
   },
